f596ca8ca9d5d9fca0d779521d7598f72d9c456d
victor <victorlopes9291@gmail.com>
2024-12-25 15:59:29 -0300

    #2 - Criando o sliding das tecnologias

---
+++ src/components/Sliding/Sliding.jsx
@@ -0,0 +1,195 @@
+import "./Sliding.css";
+
+export const Sliding = () => {
+    return (
+        <>
+            <div className="card-sliding">
+                <div className="emojis">
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=spring"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=java"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=idea"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=eclipse"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=rabbitmq"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=flutter"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=dart"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=firebase"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=cs"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=net"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=visualstudio"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=py"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=flask"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=opencv"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=selenium"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=react"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=angular"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=js"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=html"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=css"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=jquery"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=supabase"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=androidstudio"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=postgres"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=vim"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=linux"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=ubuntu"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=figma"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=vscode"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=git"/>
+                    </button>
+                </div>
+                {/*<-- Duplicação para loop contínuo -->*/}
+                <div className="emojis">
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=spring"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=java"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=cs"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=net"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=py"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=flask"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=react"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=angular"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=rabbitmq"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=js"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=html"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=css"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=jquery"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=flutter"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=dart"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=firebase"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=supabase"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=idea"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=eclipse"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=androidstudio"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=postgres"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=vim"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=linux"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=ubuntu"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=figma"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=vscode"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=git"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=visualstudio"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=opencv"/>
+                    </button>
+                    <button className="button-sliding">
+                        <img src="https://skillicons.dev/icons?i=selenium"/>
+                    </button>
+                </div>
+            </div>
+        </>
+    )
+}
