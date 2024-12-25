27b4301e1d808de3644429a0794440e86103d872
victor <victorlopes9291@gmail.com>
2024-12-25 15:59:06 -0300

    #2 - Adicionando as saudações e card do conteúdo da página

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -2,37 +2,44 @@ import {Card} from "../Card/Card.jsx";
 import "./Content.css"
 import Lottie from "lottie-react";
 import animationData from '../../assets/lotties/animation_logo.json';
+import {Sliding} from "../Sliding/Sliding.jsx";
 
 export const Content = () => {
 
     return (
         <>
             <div style={{
-                margin: "15px",
+                margin: "auto",
                 display: "flex",
                 flexDirection: "column",
-                width: "100%"
+                width: "100%",
+                alignItems: "center",
+                gap: "1.5rem"
             }}>
-                <div style={{display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
-                    <h1>Hello!<br/>I'm a
-                        <div className={"wrapper"}>
-                            <span> Mobile </span>
-                            <span> Front-End </span>
-                            <span> Back-End </span>
-                        </div>
-                        <br/>
-                        Developer
-                    </h1>
-                    <Lottie loop={true} animationData={animationData}/>
+                <div style={{height: "100vh",margin: "35px",display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
+                    <div style={{textAlign: "start"}}>
+                        <h1>Hello!<br/>I'm a
+                            <div className={"wrapper"}>
+                                <span> Mobile </span>
+                                <span> Front-End </span>
+                                <span> Back-End </span>
+                            </div>
+                            <br/>
+                            Developer
+                        </h1>
+                        <p>Victor Lopes</p>
+                    </div>
                     <div className={"m2"}>
                         <Lottie loop={true} animationData={animationData}/>
                     </div>
                 </div>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
-                <Card></Card>
+                <div style={{display: "flex"}}>
+                    <Card ></Card>
+                    <Card ></Card>
+                </div>
+                <Sliding/>
+                <Card height={500}></Card>
+                <Card height={100}></Card>
             </div>
         </>
     )
