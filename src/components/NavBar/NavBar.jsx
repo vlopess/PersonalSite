922b16f842f67db0709043dd96973ac4163ce457
victor <victorlopes9291@gmail.com>
2024-12-25 15:37:34 -0300

    #2 - Criando a Navbar e seus botões

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
 import {Col, Container, Navbar, Row} from "react-bootstrap";
 import './NavBar.css';
+import {NavBarButton} from "./NavBarButton/NavBarButton.jsx";
 
 export const NavBar = () => {
     return (
         <>
-            <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
+            <Navbar  className="bg-body-tertiary" fixed="top">
                 <Row style={{width: "100%"}}>
                     <Col xs={8} sm={8} md={8} lg={10}>
                         <Container>
@@ -12,8 +13,7 @@ export const NavBar = () => {
                     </Col>
                     <Col>
                         <Container>
-                            Teste <br/>
-                            Teste
+                            <NavBarButton/>
                         </Container>
                     </Col>
                 </Row>
