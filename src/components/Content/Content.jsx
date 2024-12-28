110062e3c13755dce01ced69d94a787817fcc87e
victor <victorlopes9291@gmail.com>
2024-12-28 20:17:30 -0300

    #3 - Adicionando os componentes e estilo do conteúdo principal

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,6 +3,10 @@ import "./Content.css"
 import Lottie from "lottie-react";
 import animationData from '../../assets/lotties/animation_logo.json';
 import {Sliding} from "../Sliding/Sliding.jsx";
+import {MostTech} from "../MostTech/MostTech.jsx";
+import {ContatosList} from "../ContatosList/ContatosList.jsx";
+import {Divider} from "../Divider/Divider.jsx";
+import {DropDownProjects} from "../DropDownProjects/DropDownProjects.jsx";
 
 export const Content = () => {
 
@@ -17,7 +21,7 @@ export const Content = () => {
                 alignItems: "center",
                 gap: "1.5rem"
             }}>
-                <section style={{height: "100vh", marginTop: "10rem", display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
+                <section id={"content-home"} style={{height: "100vh", marginTop: "10rem", display: "flex", flexDirection: "row", justifyContent: "space-around"}}>
                     <div style={{textAlign: "start", width: "50%"}}>
                         <h1>Hello!<br/>I'm a
                             <div className={"wrapper"}>
@@ -28,24 +32,90 @@ export const Content = () => {
                             <br/>
                             Developer
                         </h1>
-                        <p>Victor Lopes</p>
+                        <p style={{
+                            fontSize: 22,
+                            color: "grey"
+                        }}>Victor Lopes | Desenvolvedor full-stack</p>
                     </div>
                     <div className={"m2"}>
                         <Lottie loop={true} animationData={animationData}/>
                     </div>
                 </section>
-                <section style={{height: "100vh", width: "100%", gap: "1.5rem"}}>
+                <section id={"content-aboutme"} style={{height: "100vh", width: "100%", gap: "1.5rem"}}>
+                    <Divider text={"About Me"}/>
+                    <div style={{"display": "flex", gap: "inherit"}}>
+                        <Card height={275}/>
+                        <Card height={275}/>
+                    </div>
+                    <Divider text={"Estatítiscas"}/>
                     <div style={{"display": "flex", gap: "inherit"}}>
                         <Card/>
-                        <Card/>
+                        <Card>
+                            <MostTech/>
+                        </Card>
                     </div>
+                    <Divider text={"Tecnologias"}/>
                     <Sliding/>
                 </section>
-                <section style={{margin: "25px 0", width: "100%"}}>
+                <section id={"content-projects"} style={{margin: "25px 0", width: "100%"}}>
                     <div style={{display: "flex", flexDirection: "column", width: "100%"}}>
-                        <Card height={500}></Card>
-                        <div></div>
-                        <Card height={100}></Card>
+                        <Divider text={"Projetos"}/>
+                        <div className="dot"></div>
+
+                        <Card showPoint={true} height={600}>
+                            <div style={{
+                                width: "100%",
+                                height: "100%",
+                                overflow: "scroll"
+                            }}>
+                                <DropDownProjects
+                                    title={"OlymFollow: Sistema de Visualização do Quadro de Medalhas das Olimpíadas"}>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                </DropDownProjects>
+                                <DropDownProjects
+                                    title={"OlymFollow2: Sistema de Visualização do Quadro de Medalhas das Olimpíadas"}>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                </DropDownProjects>
+                                <DropDownProjects
+                                    title={"OlymFollow3: Sistema de Visualização do Quadro de Medalhas das Olimpíadas"}>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                </DropDownProjects>
+                                <DropDownProjects
+                                    title={"OlymFollow4: Sistema de Visualização do Quadro de Medalhas das Olimpíadas"}>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                </DropDownProjects>
+                                <DropDownProjects
+                                    title={"OlymFollow5: Sistema de Visualização do Quadro de Medalhas das Olimpíadas"}>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                </DropDownProjects>
+                                <DropDownProjects
+                                    title={"OlymFollow6: Sistema de Visualização do Quadro de Medalhas das Olimpíadas"}>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                </DropDownProjects>
+                                <DropDownProjects
+                                    title={"OlymFollow7: Sistema de Visualização do Quadro de Medalhas das Olimpíadas"}>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                    <p>Texto Teste</p>
+                                </DropDownProjects>
+                            </div>
+                        </Card>
+                        <Divider text={"Contato"}/>
+                        <Card id={"content-contacts"} height={100}>
+                            <ContatosList/>
+                        </Card>
                     </div>
                 </section>
             </div>
