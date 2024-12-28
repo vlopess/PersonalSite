1487f7d37091ba150fd5256bf2b52c6b2a486c9c
victor <victorlopes9291@gmail.com>
2024-12-28 20:19:23 -0300

    #3 - Adicionando atributos e estilos dinâmicos ao Card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,16 @@
 import "./Card.css"
 
-export const Card = ({children, height = 250, width = "100%"}) => {
+export const Card = ({id, children, height = 250, width = "100%", showPoint = false}) => {
     return (
         <>
             <div
+                id={id}
                 className="card-container"
                 style={{
-                height: height + "px",
-                width: width
-            }}>
+                    height: height + "px",
+                    width: width
+                }}>
+
                 <div className="container">
                     {children}
                 </div>
