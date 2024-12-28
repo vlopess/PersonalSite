09a31b5ec7d46b628e9d75bc3c4f9fa5e53a2d97
victor <victorlopes9291@gmail.com>
2024-12-28 20:11:40 -0300

    #3 - Refatorando a NavBar: modularizando os buttons

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,58 @@
 import {Navbar} from "react-bootstrap";
 import './NavBar.css';
+import {NavBarDropDownButton} from "./NavBarDropDownButton/NavBarDropDownButton.jsx";
 import {NavBarButton} from "./NavBarButton/NavBarButton.jsx";
+import Home from "../../assets/svg/home.svg"
+import Edit from "../../assets/svg/edit.svg"
+import User from "../../assets/svg/user.svg"
+import {FolderSvg} from "../SvgComponents/FolderSvg.jsx";
 
 export const NavBar = () => {
+
+    function goToHome() {
+        let y = document.getElementById("content-home").offsetTop;
+        window.scroll({
+            top: y - 160,
+            behavior: 'smooth'
+        });
+    }
+
+    function goToAboutMe() {
+        let y = document.getElementById("content-aboutme").offsetTop;
+        window.scroll({
+            top: y - 80,
+            behavior: 'smooth'
+        });
+    }
+
+    function goToProjects() {
+        let y = document.getElementById("content-projects").offsetTop;
+        window.scroll({
+            top: y - 80,
+            behavior: 'smooth'
+        });
+    }
+
+    function goToContact() {
+        let y = document.getElementById("content-contacts").offsetTop;
+        window.scroll({
+            top: y - 80,
+            behavior: 'smooth'
+        });
+    }
+
     return (
         <>
             <Navbar>
-                <NavBarButton/>
+                <div className="button-container">
+                    <NavBarButton img={Home} title={"Home"} onTap={() => goToHome()}/>
+                    <NavBarButton img={Edit} title={"About Me"} onTap={() => goToAboutMe()}/>
+                    <NavBarButton  title={"Projects"} onTap={() => goToProjects()}>
+                        <FolderSvg/>
+                    </NavBarButton>
+                    <NavBarButton img={User} title={"Contact"} onTap={() => goToContact()}/>
+                    <NavBarDropDownButton/>
+                </div>
             </Navbar>
         </>
     )
