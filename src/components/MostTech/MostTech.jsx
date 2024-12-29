e35120f651902842992c486963e75e5310983510
victor <victorlopes9291@gmail.com>
2024-12-29 11:00:45 -0300

    #4 - Refatoração e adicionando descrição em MostTech

--- src/components/MostTech/MostTech.jsx
+++ src/components/MostTech/MostTech.jsx
@@ -4,32 +4,38 @@ export const MostTech = () => {
     return (
         <div className="container-most-tech">
             <div style={{display: "flex", flexDirection: "column", width: "100%"}}>
-                <span>Principal Stack</span>
-                <div className="skill-box">
-                    <span className="title">Java</span>
-
-                    <div className="skill-bar">
-              <span className="skill-per java">
-                <span className="tooltip">38%</span>
-              </span>
+                <span>Habilidades</span>
+                <div style={{display: "flex", width: "100%", justifyContent: "space-between"}}>
+                    <p>Mobile:</p>
+                    <div className="skill-box">
+                        <span className="title">Dart (Flutter)</span>
+                        <div className="skill-bar">
+                      <span className="skill-per dart">
+                        <span className="tooltip">38%</span>
+                      </span>
+                        </div>
                     </div>
                 </div>
-                <div className="skill-box">
-                    <span className="title">Dart</span>
-
-                    <div className="skill-bar">
-      <span className="skill-per dart">
-        <span className="tooltip">38%</span>
-      </span>
+                <div style={{display: "flex", width: "100%", justifyContent: "space-between", alignContent: "center"}}>
+                    <p>FrontEnd:</p>
+                    <div className="skill-box">
+                        <span className="title">Javascript (React)</span>
+                        <div className="skill-bar">
+                      <span className="skill-per js">
+                        <span className="tooltip">24%</span>
+                      </span>
+                        </div>
                     </div>
                 </div>
-                <div className="skill-box">
-                    <span className="title">Javascript</span>
-
-                    <div className="skill-bar">
-      <span className="skill-per js">
-        <span className="tooltip">24%</span>
-      </span>
+                <div style={{display: "flex", width: "100%", justifyContent: "space-between"}}>
+                    <p>BackEnd:</p>
+                    <div className="skill-box">
+                        <span className="title">Java (Spring Boot)</span>
+                        <div className="skill-bar">
+                        <span className="skill-per java">
+                            <span className="tooltip">38%</span>
+                        </span>
+                        </div>
                     </div>
                 </div>
             </div>
