778875c8e48d199d2d9d2dc47bc1372d61f21ae0
victor <victorlopes9291@gmail.com>
2025-01-01 21:27:54 -0300

    #6 - Adicionando video ao slide

--- src/components/SlideShow/SlideItem/SlideItem.jsx
+++ src/components/SlideShow/SlideItem/SlideItem.jsx
@@ -1,11 +1,14 @@
 import "../SlideShow.css";
 
-export const SlideItem = ({imgUrl, text}) => {
+export const SlideItem = ({id, imgUrl, videoUrl}) => {
     return (
         <>
-            <div className="mySlides fade">
-                <img src={imgUrl} style={{width:"100%"}} alt=""/>
-                <div className="text">{text}</div>
+            <div className={"mySlides fade " + id}>
+                <img src={imgUrl} alt=""/>
+                {videoUrl && (<video width="400" height="300" controls>
+                    <source src={videoUrl} type="video/mp4"/>
+                    Your browser does not support HTML video.
+                </video>)}
             </div>
         </>
     )
