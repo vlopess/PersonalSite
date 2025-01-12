52c0a1fb6c0eb03782ddf75ce026bdb2ef7b609e
victor <victorlopes9291@gmail.com>
2025-01-12 16:37:45 -0300

    #8 - Ajuste na ordem da linha do tempo

--- src/components/TimeLine/TimeLine.jsx
+++ src/components/TimeLine/TimeLine.jsx
@@ -11,10 +11,10 @@ export const TimeLine = () => {
                 <h2><Translator path="aboutMe.timeLine.title"/></h2>
                 <ul className="timeLine">
                     <TimeLineCertificate data={"2024/12 -->"} text={"Introdução a Teste de Software (UFBA)"}/>
-                    <TimeLineCertificate data={"2024/03 -->"} text={"The Complete Spring Boot Development Bootcamp (Udemy)"}/>
                     <TimeLineCertificate data={"2024/04 -->"} text={"Formação Spring Boot 3 (Alura)"}/>
-                    <TimeLineCertificate data={"2023/07 -->"} text={"Curso de Java encontra Scrum: Desenvolvendo software com Agilidade (UFBA)"}/>
-                    <TimeLineCertificate data={"2023/08 -->"} text={"Curso de Desenvolvimento de Aplicativos Móveis (UFBA)"}/>
+                    <TimeLineCertificate data={"2024/03 -->"} text={"The Complete Spring Boot Development Bootcamp (Udemy)"}/>
+                    <TimeLineCertificate data={"2023/08 -->"} text={"Curso de Java encontra Scrum: Desenvolvendo software com Agilidade (UFBA)"}/>
+                    <TimeLineCertificate data={"2023/07 -->"} text={"Curso de Desenvolvimento de Aplicativos Móveis (UFBA)"}/>
                     <TImeLineWork data={"2023/01 -->"}  text={"Computação Brasil"}/>
                     <TimeLineCollege data={"2022/08 -->"}  text={"Análise e Desenvolvimento de Sistemas (IFBA)"}/>
                 </ul>
