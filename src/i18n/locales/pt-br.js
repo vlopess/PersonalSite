7e68de3fd8f40177533b587ec9eb8a42833c6f4b
victor <victorlopes9291@gmail.com>
2025-01-12 20:05:31 -0300

    #8 - Corrigindo erro ortográfico

--- src/i18n/locales/pt-br.js
+++ src/i18n/locales/pt-br.js
@@ -44,7 +44,7 @@ export default {
                 text : 'Goth4Goth é um aplicativo de chat em tempo real criado para a comunidade gótica e alternativa, onde os usuários podem se conectar anonimamente e discutir seus interesses em comum. Com salas públicas e privadas, Goth4Goth proporciona um espaço seguro e exclusivo para trocar ideias, conhecer novas pessoas e explorar temas subculturais.'
             },
             YourTaste : {
-                title : 'Your Taste Around The World: descubra a origem dos artirtas que mais escuta',
+                title : 'Your Taste Around The World: descubra a origem dos artistas que mais escuta',
                 text : 'Your Taste Around The World é um site interativo que permite aos usuários descobrir a origem dos artistas que eles mais escutam, por meio da sua conta do lastfm ou spotify. Your Taste Around The World foi feito em React que consume as API\'s do Lastfm, Spotify e Gemini para mostrar a origem dos artistas que o usuário escuta.'
             },
             VimTutorial : {
@@ -68,7 +68,7 @@ export default {
                 text : 'Este projeto é o resultado de um trabalho de conclusão do curso de Programação Orientada a Objetos (POO). O Sudoku foi desenvolvido como uma aplicação de janela (Swing), utilizando os princípios fundamentais de POO. Feito em dupla com o '
             },
             SimilarTaste : {
-                title : 'Similiar Taste: descubra artirtas semelhantes as preferências musicais',
+                title : 'Similiar Taste: descubra artistas semelhantes as preferências musicais',
                 text : 'Similiar Taste é um projeto que permite aos usuários descobrir músicas e artistas semelhantes com base em suas preferências musicais utilizando a API do Lastfm. Permite encontrar músicas semelhantes que se alinham com o gosto musical do usuário. Permite que os usuários insiram o nome de um artista que eles gostam, encontrar artistas que compartilham estilos e características musicais semelhantes com o artista inserid'
             }
         },
