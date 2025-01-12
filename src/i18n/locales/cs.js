3787d98a890c1d33404d739ec01bdf2c0d718677
victor <victorlopes9291@gmail.com>
2025-01-11 22:54:05 -0300

    #7 - Upload file of configuration czecho language

---
+++ src/i18n/locales/cs.js
@@ -0,0 +1,84 @@
+export default {
+    translations: {
+        navbar: {
+            idioma : 'Jazyk',
+            home: 'Domů',
+            aboutMe: 'O Mně',
+            projects: 'Projekty',
+            contact: 'Kontakt',
+            burguer: {
+                descricao: 'Možnosti',
+                pt: 'Portugalština',
+                en: 'Angličtina',
+                es: 'Španělština',
+                cs: 'Čeština'
+            }
+        },
+        home: {
+            dev : 'Vývojář',
+            hello: 'Ahoj!',
+            whoIam: 'Jsem',
+            welcome: 'Vítejte v mém portfoliu!'
+        },
+        aboutMe: {
+            bio: {
+                title: 'Biografie',
+                text: 'Ahoj! Jmenuji se Victor a jsem nadšenec do programování. Mám 2 roky zkušeností s vývojem softwaru, zaměřuji se na vývoj mobilních aplikací. Moje cesta začala s Flutterem a Pythonem, a profesionálně pracuji s .NET. Mám zájem o Spring Boot a React, na kterých pracuji na osobních projektech. Když neprogramuji, rád sleduji filmy.',
+            },
+            article: {
+                title: 'Poslední Články',
+                more : 'Zobrazit Více'
+            },
+            timeLine: {
+                title: 'Časová Osa'
+            },
+            habilidades: {
+                title: 'Dovednosti'
+            }
+        },
+        projeto: {
+            Goth4Goth: {
+                title: 'Goth4Goth: chat v reálném čase vytvořený pro gotickou a alternativní komunitu',
+                text: 'Goth4Goth je aplikace chatu v reálném čase vytvořená pro gotickou a alternativní komunitu, kde se uživatelé mohou anonymně spojit a diskutovat o společných zájmech. Díky veřejným a soukromým místnostem nabízí Goth4Goth bezpečný a exkluzivní prostor pro výměnu myšlenek, poznávání nových lidí a zkoumání subkulturních témat.'
+            },
+            YourTaste: {
+                title: 'Your Taste Around The World: interaktivní stránka umožňující uživatelům objevovat původ jejich oblíbených umělců',
+                text: 'Your Taste Around The World je interaktivní stránka umožňující uživatelům objevovat původ jejich oblíbených umělců prostřednictvím účtu na Last.fm nebo Spotify. Your Taste Around The World je vytvořený v Reactu a využívá API Last.fm, Spotify a Gemini pro zobrazení původu umělců, které uživatel poslouchá.'
+            },
+            VimTutorial: {
+                title: 'Vim Tutorial: kompletní a interaktivní tutoriál na textový editor Vim',
+                text: 'Toto je stránka, která nabízí kompletní a interaktivní tutoriál na textový editor Vim. Cílem je pomoci všem naučit se a ovládnout základní i pokročilé funkce Vim.'
+            },
+            MyMusicTaste: {
+                title: 'MyMusicTaste: aplikace pro hudební koláže',
+                text: 'MyMusicTaste je mobilní aplikace pro vizualizaci dat z Last.fm pomocí API Last.fm. Funguje jako klon mobilní aplikace Last.fm s některými změnami v designu a novými funkcemi. Znáte tu píseň, která vám připomíná nějaké místo nebo osobu? Tento pocit nostalgie lze zaznamenat v MyMusicTaste! Stačí propojit píseň, popis a obrázek a hotovo!'
+            },
+            Olympics: {
+                title: 'Olympics Follow: Systém pro zobrazení medailového žebříčku na olympijských hrách',
+                text: 'Systém pro zobrazení medailového žebříčku na olympijských hrách, vyvinutý pomocí Spring Boot na backendu a React.js na frontendu, implementující zasílání zpráv přes RabbitMQ pro oznámení e-mailem uživatelům, kteří se přihlásili k odběru, když jejich země získá medaili. Tento projekt byl výsledkem závěrečné práce z předmětu Webové Programování, realizovaný ve dvojici'
+            },
+            LeChat: {
+                title: 'LeChat: mobilní aplikace pro vytváření konverzačních skupin',
+                text: 'Le Chat je mobilní aplikace pro vytváření konverzačních skupin. Existuje také verze'
+            },
+            Sudoku: {
+                title: 'Sudoku v Javě',
+                text: 'Tento projekt je výsledkem závěrečné práce z kurzu Objektově Orientované Programování (OOP). Sudoku bylo vyvinuto jako oknová aplikace (Swing) pomocí základních principů OOP. Realizováno ve dvojici'
+            },
+            SimilarTaste: {
+                title: 'Similar Taste: projekt umožňující uživatelům objevovat podobné písně a umělce podle jejich hudebního vkusu',
+                text: 'Similar Taste je projekt umožňující uživatelům objevovat podobné písně a umělce podle jejich hudebního vkusu pomocí API Last.fm. Pomáhá najít hudbu, která odpovídá jejich vkusu. Uživatelé mohou zadat jméno umělce, kterého mají rádi, a najít umělce, kteří sdílejí podobné styly a vlastnosti s vloženým umělcem.'
+            }
+        },
+        divider: {
+            aboutMe: 'O Mně',
+            tech: 'Nástroje a Technologie',
+            projetos: 'Projekty',
+            contato: 'Kontakt'
+        },
+        button: {
+            github: 'Zobrazit na Githubu',
+            web: 'Zobrazit na Webu'
+        }
+    }
+}
