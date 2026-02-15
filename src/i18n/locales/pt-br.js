1f2828a3ee26db7dbb2ee34b2adc4f99dd7d262b
Victor Lopes <121266927+vlopess@users.noreply.github.com>
2026-02-15 11:43:41 -0300

    Add LanguageToday description in Portuguese locale
    Added description for LanguageToday application.

--- src/i18n/locales/pt-br.js
+++ src/i18n/locales/pt-br.js
@@ -39,6 +39,10 @@ export default {
             }
         },
         projeto : {
+            languageToday : {
+                title : 'LanguageToday',
+                text : 'LanguageToday é um aplicativo web educacional gratuito desenvolvido para ajudar os alunos a estudar inglês e tcheco por meio de lições estruturadas, exercícios práticos e orientação assistida por inteligência artificial.'
+            },
             pixelguess : {
                 title : 'PixelGuess',
                 text : 'PixelGuess é um jogo de adivinhação visual em que o jogador tenta identificar uma imagem enquanto ela gradualmente deixa de ser pixelizada.'
